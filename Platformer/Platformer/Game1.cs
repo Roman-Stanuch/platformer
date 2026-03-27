70ac661e978ff237f6eb1d8a2323882542d84421
Polygonal Graphics <154336865+Roman-Stanuch@users.noreply.github.com>
2026-03-27 07:42:09 -0500

    Added animation

--- Platformer/Platformer/Game1.cs
+++ Platformer/Platformer/Game1.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Graphics;
 
 namespace Platformer
 {
@@ -8,6 +10,8 @@ namespace Platformer
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        private AnimatedSprite _adventurer;
 
         public Game1()
         {
@@ -27,7 +31,42 @@ namespace Platformer
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            Texture2DAtlas atlas = Content.Load<Texture2DAtlas>("sprites/spritesheet");
+            SpriteSheet spriteSheet = new SpriteSheet("Spritesheet/adventurer", atlas);
+
+            TimeSpan duration = TimeSpan.FromSeconds(0.1);
+            spriteSheet.DefineAnimation("attack", builder =>
+            {
+                builder.IsLooping(false)
+                       .AddFrame("adventurer-attack3-00", duration)
+                       .AddFrame("adventurer-attack3-01", duration)
+                       .AddFrame("adventurer-attack3-02", duration)
+                       .AddFrame("adventurer-attack3-03", duration)
+                       .AddFrame("adventurer-attack3-04", duration)
+                       .AddFrame("adventurer-attack3-05", duration);
+            });
+
+            spriteSheet.DefineAnimation("idle", builder =>
+            {
+                builder.IsLooping(true)
+                       .AddFrame("adventurer-idle-2-00", duration)
+                       .AddFrame("adventurer-idle-2-01", duration)
+                       .AddFrame("adventurer-idle-2-02", duration)
+                       .AddFrame("adventurer-idle-2-03", duration);
+            });
+
+            spriteSheet.DefineAnimation("run", builder =>
+            {
+                builder.IsLooping(true)
+                       .AddFrame("adventurer-run-00", duration)
+                       .AddFrame("adventurer-run-01", duration)
+                       .AddFrame("adventurer-run-02", duration)
+                       .AddFrame("adventurer-run-03", duration)
+                       .AddFrame("adventurer-run-04", duration)
+                       .AddFrame("adventurer-run-05", duration);
+            });
+
+            _adventurer = new AnimatedSprite(spriteSheet, "idle");
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,6 +75,7 @@ namespace Platformer
                 Exit();
 
             // TODO: Add your update logic here
+            _adventurer.Update(gameTime);
 
             base.Update(gameTime);
         }
@@ -44,7 +84,10 @@ namespace Platformer
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+            int scale = 3;
+            _spriteBatch.Draw(_adventurer, _adventurer.Origin * scale, 0, new Vector2(scale));
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
